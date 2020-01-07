eb8ef14fbd1b0d5a4094c8bbcaba0c83b558a8b4
Panadda Manarata <manarata.panadda@gmail.com>
2020-01-07 01:27:54 +0100

    today

---
+++ js/carousel.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+        clickable: true,
+        centeredSlides: true,
+        centeredSlidesBounds: true,
+        fadeEffect: {
+            crossFade: true
+        },
+    },
+});
